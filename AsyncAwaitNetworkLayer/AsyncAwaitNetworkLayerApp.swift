0f739fa73cf751fb79e1d9fc5ffe96f7426b4983
pradeepiOS <pradeep.iostech@gmail.com>
2021-12-02 15:13:32 +0530

    Initial Commit

---
+++ AsyncAwaitNetworkLayer/AsyncAwaitNetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitNetworkLayerApp.swift
+//  AsyncAwaitNetworkLayer
+//
+//  Created by Pradeep's Macbook on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitNetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
